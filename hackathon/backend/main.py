c4061b5778036fe5330ce07f14ad6ce9ab010a81
SohanBC <sohanbc.cs24@bmsce.ac.in>
2025-11-21 12:39:31 +0530

    Add files via upload

--- hackathon/backend/main.py
+++ hackathon/backend/main.py
@@ -4,3 +4,7 @@ from backend.routes.scan_url import router as scan_url_router
 
 app = FastAPI(title="Fake App Detection API")
 app.include_router(scan_url_router, prefix="/api")
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app , port=8000)
